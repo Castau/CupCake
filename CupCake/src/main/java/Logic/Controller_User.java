6ab589185e6293be8578b3b3cdae029716a677ca
HrBjarup <asger@bjarup.dk>
2019-02-28 20:38:24 +0100

    Added password checking method in logic.Controller_User.java

--- CupCake/src/main/java/Logic/Controller_User.java
+++ CupCake/src/main/java/Logic/Controller_User.java
@@ -1,13 +1,27 @@
 package Logic;
 
+import Data.Mapper_User;
+import Data.Model_User;
+import java.sql.SQLException;
+
 /**
  * @author Camilla
  */
-public class Controller_User {
+public class Controller_User 
+{
 
-    public Controller_User() {
+    public Controller_User() 
+    {
         
-        
+    }
+    
+    public boolean checkPasswordValidity(String name, String password) throws SQLException
+    {
+        String passwordDB ="";
+        Mapper_User userDAO = new Mapper_User();
+        Model_User user = userDAO.getUserByName(name);
+        passwordDB = user.getPassword();
+        return password.equals(passwordDB);
     }
     
 }
