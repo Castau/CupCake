3e83b2af67dcd41fbc6c5d6e29897fe5f342f479
Runi-VN <37186286+Runi-VN@users.noreply.github.com>
2019-02-28 15:03:21 +0100

    fix 404 error page

--- CupCake/src/main/java/Presentation/unknownCMD.java
+++ CupCake/src/main/java/Presentation/unknownCMD.java
@@ -15,24 +15,28 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author stein
  */
+public class unknownCMD extends Command
+{
 
-
-public class unknownCMD extends Command {
-     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         response.setContentType("text/html;charset=UTF-8");
-               
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Error</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Error</h1>");
-                out.println("</body>");
-                out.println("</html>");
-            }
+
+        try (PrintWriter out = response.getWriter())
+        {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>ERROR 404</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Error 404 - no such webpage</h1>");
+            out.println("<h2>Unknown page/path: " + request.getPathInfo() + "</h2>");
+            out.println("<a href=\"/CupCake/login\"><h2>Start over</h2></a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
 }
