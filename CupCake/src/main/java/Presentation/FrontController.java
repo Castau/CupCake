5b54bb33232b8f6e59cd399cd139508645ca40c2
Runi-VN <37186286+Runi-VN@users.noreply.github.com>
2019-02-28 14:33:40 +0100

    Frontcontroller fix

--- CupCake/src/main/java/Presentation/FrontController.java
+++ CupCake/src/main/java/Presentation/FrontController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "FrontController", urlPatterns =
 {
-    "/"
+    "/*"
 })
 public class FrontController extends HttpServlet
 {
@@ -38,10 +38,10 @@ public class FrontController extends HttpServlet
     {
         try
         {
-            //Command c = Command.from(request);
-            //c.execute(request, response);
+            Command c = Command.from(request);
+            c.execute(request, response);
         } 
-        catch (Exception e)
+        catch (IOException | ServletException ex)
         {
             response.setContentType("text/html;charset=UTF-8");
             try (PrintWriter out = response.getWriter())
@@ -52,7 +52,7 @@ public class FrontController extends HttpServlet
                 out.println("<title>Error</title>");
                 out.println("</head>");
                 out.println("<body>");
-                out.println("<h1>Error: " + "</h1>");
+                out.println("<h1>Error: " + ex + "</h1>");
                 out.println("<p>Bad URL</p>");
                 out.println("</body>");
                 out.println("</html>");
