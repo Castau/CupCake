ad0713ab202e01d3e597490ae9bd1aa11a6ef5ae
Steingrimurjonsson <steingrimurkj@gmail.com>
2019-02-28 13:52:08 +0100

    added pres

---
+++ CupCake/src/main/java/Presentation/Command.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author stein
+ */
+public abstract class Command {
+
+    public abstract void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+    public static Command from(HttpServletRequest request){
+        Command command;
+        String path = request.getPathInfo().substring(1);
+        //String path = request.getParameter("path");
+
+        switch (path){
+            case "":
+                command = new loginCMD();
+                break;
+            case "shop":
+                command = new shopCMD();
+                break;
+            case "cart": 
+                command = new shopCartCMD();
+                break;
+            case "customer": 
+                command = new customerCMD();
+                break;
+            default: 
+                command = new UnknownCommand();
+        }
+        return command;
+    }
+}
