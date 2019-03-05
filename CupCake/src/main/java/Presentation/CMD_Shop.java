205773756e62aef24d91824d8d1c241344abc247
HrBjarup <asger@bjarup.dk>
2019-03-05 11:57:08 +0100

    Fixed Mapper_CupCake

--- CupCake/src/main/java/Presentation/CMD_Shop.java
+++ CupCake/src/main/java/Presentation/CMD_Shop.java
@@ -5,8 +5,14 @@
  */
 package Presentation;
 
+import Data.Mapper_CupCake;
+import Data.Model_CupCake;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,22 +21,50 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author stein
  */
-public class CMD_Shop extends Command {
-     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class CMD_Shop extends Command
+{
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         response.setContentType("text/html;charset=UTF-8");
-               
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Shop</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Shop</h1>");
-                out.println("</body>");
-                out.println("</html>");
+
+        Mapper_CupCake mc = new Mapper_CupCake();
+        ArrayList<Model_CupCake> cupcakes = new ArrayList();
+        try
+        {
+            cupcakes = mc.getAllCupCakes();
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(CMD_Shop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try (PrintWriter out = response.getWriter())
+        {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Shop</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Shop</h1>");
+            out.println("<br/>");
+            for (int i = 0; i < cupcakes.size(); i++)
+            {
+                double total = cupcakes.get(i).getTopPrice() + cupcakes.get(i).getBottomPrice();
+                out.println("<p>"
+                        + "Cake top name: " + cupcakes.get(i).getTopName()
+                        + " price: " + cupcakes.get(i).getTopPrice()
+                        + "<br/>"
+                        + "Cake bottom name: " + cupcakes.get(i).getBottomName()
+                        + " price: " + cupcakes.get(i).getBottomPrice()
+                        + "<br/>"
+                        + "Total price: " + total
+                        + "</p>");
+                out.println("<br/>");
             }
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
 }
